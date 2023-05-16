71be65a56212345f48686e49f029e662c3e7ca99
Anushka Verma <anushkaverma779@gmail.com>
2023-05-16 17:41:04 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 //your JS code here. If required.
+  let input = document.querySelector("#ip");
+      const button = document.querySelector("#btn");
+      let output = document.querySelector("#output");
+
+      //function promise chaining
+      function PromiseChaining() {
+        const myPromise = new Promise((res) => {
+          setTimeout(() => {
+            res(input.value);
+          }, 2000);
+        });
+        myPromise
+          .then((result) => {
+            output.textContent = `Result: ${result}`;
+            return result;
+          })
+          .then((result) => {
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result * 2);
+              }, 1000);
+            });
+          })
+          .then((result) => {
+            output.textContent = `Result: ${result}`;
+            return result;
+          })
+          .then((result) => {
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result - 3);
+              }, 1000);
+            });
+          })
+          .then((result) => {
+            output.textContent = `Result: ${result}`;
+            return result;
+          })
+          .then((result) => {
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result / 2);
+              }, 1000);
+            });
+          })
+          .then((result) => {
+            output.textContent = `Result: ${result}`;
+            return result;
+          })
+          .then((result) => {
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result + 10);
+              }, 1000);
+            });
+          })
